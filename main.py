e929257fdca2c46b54dde9df04ece4e4eec9e179
KiralyCraft <alexucu@gmail.com>
2025-08-08 15:12:00 +0300

    Fix main.py to refer the submodule package

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import os
 
 qkdgkt_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'QKD-Infra-GetKey'))
 sys.path.append(qkdgkt_path)
-import qkdgkt
+from qkdgkt import qkdgkt
 import json
 
 from PyQt5.QtGui import QFontMetrics
